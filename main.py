888ecc0f49edb589b8c2747e90810a1a3ca7c057
Chad Homan <choman@gmail.com>
2020-12-12 15:55:29 -0600

    add ROM

--- main.py
+++ main.py
@@ -6,13 +6,22 @@
 #
 #
 # Create a program with the following requirements:
-#    Using comments, create a header at the top of the program indicating the purpose of the program, assignment number, and your name. Refer to the submission instructions for an example of a header.
-#    Display a welcome message for your user.
-#    Retrieve the company name from the user.
-#    Retrieve the number of feet of fiber optic cable to be installed from the user.
-#    Calculate the installation cost of fiber optic cable by multiplying the total cost as the number of feet times $0.87.
-#    Print a receipt for the user including the company name, number of feet of fiber to be installed, the calculated cost, and total cost in a legible format.
-#    Include appropriate comments throughout  program.
+#  * Using comments, create a header at the top of the program indicating the purpose of
+#    the program, assignment number, and your name. Refer to the submission instructions
+#    for an example of a header.
+#  * Display a welcome message for your user.
+#  * Retrieve the company name from the user.
+#  * Retrieve the number of feet of fiber optic cable to be installed from the user.
+#  * Calculate the installation cost of fiber optic cable by multiplying the total cost
+#    as the number of feet times $0.87.
+#  * Print a receipt for the user including the company name, number of feet of fiber to
+#    be installed, the calculated cost, and total cost in a legible format.
+#  * Include appropriate comments throughout  program.
+#
+# Record Of Modifications
+#    Author         Date            Description
+#  ----------    ------------       ----------------------------------
+#  Chad Homan     2020-12-09        Flush out issues with receipt display
 
 import datetime
 import sys
