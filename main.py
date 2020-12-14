0a9ecd1f8acb2d12eb2a164b82e3f98173ba8612
Chad Homan <choman@gmail.com>
2020-12-14 07:57:00 -0600

    fixed comments

--- main.py
+++ main.py
@@ -6,20 +6,25 @@
 #
 #
 # Create a program with the following requirements:
-#    Using comments, create a header at the top of the program indicating the purpose of the program, assignment number, and your name. Refer to the submission instructions for an example of a header.
-#    Display a welcome message for your user.
-#    Retrieve the company name from the user.
-#    Retrieve the number of feet of fiber optic cable to be installed from the user.
-#    Calculate the installation cost of fiber optic cable by multiplying the total cost as the number of feet times $0.87.
-#    Print a receipt for the user including the company name, number of feet of fiber to be installed, the calculated cost, and total cost in a legible format.
-#    Include appropriate comments throughout  program.
+#  * Using comments, create a header at the top of the program indicating the purpose of
+#    the program, assignment number, and your name. Refer to the submission instructions
+#    for an example of a header.
+#  * Display a welcome message for your user.
+#  * Retrieve the company name from the user.
+#  * Retrieve the number of feet of fiber optic cable to be installed from the user.
+#  * Calculate the installation cost of fiber optic cable by multiplying the total cost
+#    as the number of feet times $0.87.
+#  * Print a receipt for the user including the company name, number of feet of fiber to
+#    be installed, the calculated cost, and total cost in a legible format.
+#  * Include appropriate comments throughout  program.
 #
-#  Record of Modifications:
+# Record Of Modifications
+#    Author         Date            Description
+#  ----------    ------------       ----------------------------------
+#  Chad Homan     2020-12-14        Added initial discount code
+#  Chad Homan     2020-12-09        Flush out issues with receipt display
 #
-#    Author         Date         Description
-#   ---------     ----------     -
-#   Chad Homan    2020-12-14
-#
+
 import datetime
 import sys
 
