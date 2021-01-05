2efe8bcbcea8c8e1d827ba6597cc6a2ddd2d0de5
Chad Homan <choman@gmail.com>
2021-01-05 12:25:37 -0600

    updated comments

--- calc.py
+++ calc.py
@@ -4,35 +4,38 @@
 # Author: Chad Homan
 # 2021/01/05
 #
-# Abstract: This programming takes in basic company info and lenth of fiber cable. 
-#           Calculates the cost of the cable based on length and if discounts apply.
-#           Then displays a receipt for the company's purchase.
+# Abstract: This program performs basic math and averages based on user
+#           input. 
 #
 # Create a program with the following requirements:
 #
 #  * Your program must have a header.
-#  * This program will perform various calculations (addition, subtraction,
-#    multiplication, division, and average calculation)
+#  * This program will perform various calculations (addition, 
+#    subtraction, multiplication, division, and average calculation)
 #  * This program will contain a variety of loops and functions.
-#  * The program will add, subtract, multiply, divide two numbers and provide
-#    the average of multiple numbers input by the user.
+#  * The program will add, subtract, multiply, divide two numbers and
+#    provide the average of multiple numbers input by the user.
 #  * Define a function named performCalculation which takes one parameter.
 #    The parameter will be the operation being performed (+, -, *, /).
-#     - This function will perform the given prompt the user for two numbers then
-#       perform the expected operation depending on the parameter that's passed
-#       into the function.
+#     - This function will perform the given prompt the user for two 
+#       numbers then perform the expected operation depending on the
+#       parameter that's passed into the function.
 #     - This function will print the calculated value for the end user.
 #  * Define a function named calculateAverage which takes no parameters.
-#      - This function will ask the user how many numbers they wish to input.
-#      - This function will use the number of times to run the program within a
-#        for loop in order to calculate the total and average.
+#      - This function will ask the user how many numbers they wish to 
+#        input.
+#      - This function will use the number of times to run the program 
+#        within a for loop in order to calculate the total and average.
 #      - This function will print the calculated average.
-#  * This program will have a main section which contains a while loop. The while
-#    loop will be used to allow the user to run the program until they enter a
-#    value which ends the loop.
-#  * The main program should prompt the user for the operation they wish to perform.
-#  * The main program should evaluate the entered data using if statements.
-#  * The main program should call the necessary function to perform the calculation.
+#  * This program will have a main section which contains a while loop. 
+#    The while loop will be used to allow the user to run the program 
+#    until they enter a value which ends the loop.
+#  * The main program should prompt the user for the operation they wish
+#    to perform.
+#  * The main program should evaluate the entered data using if 
+#    statements.
+#  * The main program should call the necessary function to perform the 
+#    calculation.
 #
 # Record Of Modifications
 #    Author         Date            Description
@@ -42,6 +45,7 @@
 
 import datetime
 import sys
+
 
 def main():
     pass
