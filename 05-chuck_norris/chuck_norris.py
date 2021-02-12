b5216fda8dc14750acc528819119f792dafe0aa0
Chad Homan <choman@gmail.com>
2021-02-12 12:54:18 -0600

    started coding

--- 05-chuck_norris/chuck_norris.py
+++ 05-chuck_norris/chuck_norris.py
@@ -11,19 +11,19 @@
 # Create a program with the following requirements:
 #   - Create a program which uses the Request library to make a GET request
 #     of the following API: Chuck Norris Jokes.
-#   - The program will receive a JSON response which includes various piecess
-#     of data. You should parse the JSON data to obtain the “value” key. Thes
-#     data associated with the value key should be displayed for the users
+#   - The program will receive a JSON response which includes various pieces
+#     of data. You should parse the JSON data to obtain the “value” key. The
+#     data associated with the value key should be displayed for the user
 #     (i.e., the joke).
-#   - Your program should allow the user to request a Chuck Norris joke ass
+#   - Your program should allow the user to request a Chuck Norris joke as
 #     many times as they would like. You should make sure that your programs
-#     does error checking at this point. If you ask the user to enter “Y” ands
-#     they enter y, is that ok? Does it fail? If it fails, display a messages
-#     for the user. There are other ways to handle this. Think about includeds
+#     does error checking at this point. If you ask the user to enter “Y” and
+#     they enter y, is that ok? Does it fail? If it fails, display a message
+#     for the user. There are other ways to handle this. Think about included
 #     string functions you might be able to call.
 #   - Your program must include a header as in previous weeks.
 #   - Your program must include a welcome message for the user.
-#   - Your program must generate “pretty” output. Simply dumping a bunch ofs
+#   - Your program must generate “pretty” output. Simply dumping a bunch of
 #     data to the screen with no context doesn’t represent “pretty.”
 #
 # Record Of Modifications
@@ -35,11 +35,37 @@
 import requests
 import string
 
+QUIT = "n"
+URL  = "https://api.chucknorris.io/jokes/random"
 
 # function: main()
 # abstract: Main program
 #
 def main():
+    welcome()
+    while True:
+        result = input("Would you like to here a Chuck Norris joke [Y/n]? ")
+        exit_program(result)
+        get_joke()
+
+# function: get_joke()
+# abstract: get a chuck norris joke
+#
+def get_joke():
+    result = request
+
+# function: welcome()
+# abstract: welcome message
+#
+def welcome():
+    pass
+
+# function: exit_program()
+# abstract: exit program
+#
+def exit_program(result):
+    if result.lower() in QUIT:
+        sys.exit()
 
 if __name__ == "__main__":
     try:
