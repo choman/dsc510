3990b711ed10b5a3ad82a64f4274cc7e8e64e0f2
Chad Homan <choman@gmail.com>
2021-03-02 17:43:47 -0600

    update

--- term/test_weather.py
+++ term/test_weather.py
@@ -8,27 +8,27 @@ import weather
 class TestWindDirection(unittest.TestCase):
 
     def test_90_directions(self):
-        self.assertEqual(weather.getWindDirection(360), "N")
-        self.assertEqual(weather.getWindDirection(0),   "N")
-        self.assertEqual(weather.getWindDirection(90),  "E")
-        self.assertEqual(weather.getWindDirection(180), "S")
-        self.assertEqual(weather.getWindDirection(270), "W")
+        self.assertEqual(weather.getWindDirection(360).split()[0], "N")
+        self.assertEqual(weather.getWindDirection(0).split()[0],   "N")
+        self.assertEqual(weather.getWindDirection(90).split()[0],  "E")
+        self.assertEqual(weather.getWindDirection(180).split()[0], "S")
+        self.assertEqual(weather.getWindDirection(270).split()[0], "W")
 
     def test_45_directions(self):
-        self.assertEqual(weather.getWindDirection(45),  "NE")
-        self.assertEqual(weather.getWindDirection(135), "SE")
-        self.assertEqual(weather.getWindDirection(225), "SW")
-        self.assertEqual(weather.getWindDirection(315), "NW")
+        self.assertEqual(weather.getWindDirection(45).split()[0],  "NE")
+        self.assertEqual(weather.getWindDirection(135).split()[0], "SE")
+        self.assertEqual(weather.getWindDirection(225).split()[0], "SW")
+        self.assertEqual(weather.getWindDirection(315).split()[0], "NW")
 
     def test_22_5_directions(self):
-        self.assertEqual(weather.getWindDirection(22.5),  "NNE")
-        self.assertEqual(weather.getWindDirection(67.5),  "ENE")
-        self.assertEqual(weather.getWindDirection(112.5), "ESE")
-        self.assertEqual(weather.getWindDirection(157.5), "SSE")
-        self.assertEqual(weather.getWindDirection(202.5), "SSW")
-        self.assertEqual(weather.getWindDirection(247.5), "WSW")
-        self.assertEqual(weather.getWindDirection(292.5), "WNW")
-        self.assertEqual(weather.getWindDirection(337.5), "NNW")
+        self.assertEqual(weather.getWindDirection(22.5).split()[0],  "NNE")
+        self.assertEqual(weather.getWindDirection(67.5).split()[0],  "ENE")
+        self.assertEqual(weather.getWindDirection(112.5).split()[0], "ESE")
+        self.assertEqual(weather.getWindDirection(157.5).split()[0], "SSE")
+        self.assertEqual(weather.getWindDirection(202.5).split()[0], "SSW")
+        self.assertEqual(weather.getWindDirection(247.5).split()[0], "WSW")
+        self.assertEqual(weather.getWindDirection(292.5).split()[0], "WNW")
+        self.assertEqual(weather.getWindDirection(337.5).split()[0], "NNW")
 
     def test_format_single_word_title(self):
         test_string = "wind"
